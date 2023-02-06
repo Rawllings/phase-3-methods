d194caf0fbf13e1694ab6f86d6fc6dcefa443833
Rawllings Kiprop <rutojr01@gmail.com>
2023-02-06 18:33:09 +0300

    Completed task

--- methods.rb
+++ methods.rb
@@ -1,1 +1,26 @@
 # Your code here!
+
+def greet_programmer 
+    #puts "Hello, #{name}!"
+    puts "Hello, programmer!"
+end
+
+def greet (name)
+   puts "Hello, #{name}!"
+end
+
+def greet_with_default (name="programmer")
+    puts "greet_with_default: Hello, #{name}!"
+end
+
+
+
+def add (a, b)
+    a + b
+end
+
+def halve(a)
+    return nil unless a.class == Integer
+    a / 2
+end
+
